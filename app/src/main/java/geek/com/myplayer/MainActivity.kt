92525a18e5b2f64a253cdf7f5e55d7d92065ded9
henna_dev <hennasingh04@gmail.com>
2018-12-19 12:49:52 +0000

    Update MainActivity.kt
    Practicing functions in Kotlin

--- app/src/main/java/geek/com/myplayer/MainActivity.kt
+++ app/src/main/java/geek/com/myplayer/MainActivity.kt
@@ -2,11 +2,17 @@ package geek.com.myplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myToast("Hello World")
+    }
+
+    private fun myToast(text: String) { // after : is return value
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
