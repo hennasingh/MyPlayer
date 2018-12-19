db04c1d8bf6e5155250130fd60c41a44201cd814
henna_dev <hennasingh04@gmail.com>
2018-12-19 17:23:54 +0000

    Kotlin functions and variables
    Learned about calling functions in Kotlin and different variables var and val , how to write return types

--- app/src/main/java/geek/com/myplayer/MainActivity.kt
+++ app/src/main/java/geek/com/myplayer/MainActivity.kt
@@ -2,7 +2,9 @@ package geek.com.myplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,9 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         myToast("Hello World")
-    }
+
+      }
 
     private fun myToast(text: String) { // after : is return value
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
+
+    fun getInt(int: Int) : Int = 2*int
+
+    fun test() {
+        val x:Int = 20
+        val y:Long = x.toLong()
+        var string = "hello"
+        string = "GoodBye"
+    }
+
 }
