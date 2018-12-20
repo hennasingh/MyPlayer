11ce57747bd929881ab51a3f6ff4f57d18aa80dc
henna_dev <hennasingh04@gmail.com>
2018-12-20 15:47:54 +0000

    Update Test.kt
    default values and constructor overloading

--- app/src/main/java/geek/com/myplayer/Test.kt
+++ app/src/main/java/geek/com/myplayer/Test.kt
@@ -1,6 +1,6 @@
 package geek.com.myplayer
 
-open class Person (name:String, val age: Int){
+open class Person (name:String = "Peter", val age: Int = 10){
     var name: String = name
     get() ="Name: $field"
 
@@ -17,4 +17,8 @@ fun test(){
     val person = Person("John", 30)
     val age = person.age
     val name = person.name
+
+    val p2 = Person("Tom") // will create person with name Tom and age 10 (default value)
+    val p3 = Person(age = 20)
+    val p4 = Person(age=30, name="Thomas") //makes use of constructor overloading here, this works in Kotlin
 }
