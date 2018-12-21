44e1d93f716d22d555a8790ed1778ada5b5850cd
henna_dev <hennasingh04@gmail.com>
2018-12-21 11:16:47 +0000

    Implemented bind function of RV Adapter
    created a separate MediaItem data class and added dependency for Picasso, Internet permission and ids of Views in XML and finally bind function in adapter

--- app/src/main/java/geek/com/myplayer/MediaAdapter.kt
+++ app/src/main/java/geek/com/myplayer/MediaAdapter.kt
@@ -4,11 +4,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.view_media_item.view.*
 import java.util.zip.Inflater
 
-class MediaItem
-
-class MediaAdapter(val items: List<MediaItem>) : RecyclerView.Adapter<MediaAdapter.MediaViewHolder>() {
+class MediaAdapter(private val items: List<MediaItem>) : RecyclerView.Adapter<MediaAdapter.MediaViewHolder>() {
 
     override fun onCreateViewHolder(container: ViewGroup, viewType: Int): MediaViewHolder {
         val view = LayoutInflater.from(container.context).inflate(R.layout.view_media_item, container, false)
@@ -26,7 +26,8 @@ class MediaAdapter(val items: List<MediaItem>) : RecyclerView.Adapter<MediaAdapt
     class MediaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
         fun bind(item: MediaItem){
-
+            itemView.titleView.text = item.title
+            Picasso.get().load(item.thumbUrl).into(itemView.thumbUrlView)
         }
     }
 }
---
+++ app/src/main/java/geek/com/myplayer/MediaItem.kt
@@ -0,0 +1,3 @@
+package geek.com.myplayer
+
+data class MediaItem(val title: String, val thumbUrl: String)
