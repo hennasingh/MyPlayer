2b3a72185b3c0a65d6e97c2d17f58e09e2aaf571
henna_dev <hennasingh04@gmail.com>
2018-12-23 23:43:22 +0000

    Implemented Enums

--- app/src/main/java/geek/com/myplayer/MediaItem.kt
+++ app/src/main/java/geek/com/myplayer/MediaItem.kt
@@ -1,3 +1,5 @@
 package geek.com.myplayer
 
-data class MediaItem(val title: String, val thumbUrl: String)
+data class MediaItem(val title: String, val thumbUrl: String, val type: TYPE){
+    enum class TYPE { PHOTO, VIDEO}
+}
