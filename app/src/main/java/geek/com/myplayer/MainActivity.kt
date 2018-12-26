392a3ba257122f72e2a25d89ce23fe4b22d6390b
henna_dev <hennasingh04@gmail.com>
2018-12-26 21:25:19 +0000

    Update MainActivity.kt
    Implemented lambdas and generics in apply function

--- app/src/main/java/geek/com/myplayer/MainActivity.kt
+++ app/src/main/java/geek/com/myplayer/MainActivity.kt
@@ -3,6 +3,7 @@ package geek.com.myplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +17,17 @@ class MainActivity : AppCompatActivity() {
                 mediaItem ->  toast(mediaItem.title)
         }
 
+        val textView = TextView(this).apply2 {
+            text = "Hello"
+            textSize = 20f
+        }
 
-     }
+      }
+
+    fun <T> T.apply2(f :T.() -> Unit):T {
+        this.f()
+        return this
+    }
 
 }
 
